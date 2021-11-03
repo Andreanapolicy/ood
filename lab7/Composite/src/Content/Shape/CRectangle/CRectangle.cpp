c72be5b05da01517ae16991302a192bb3f3ab814
Andrey Dudin <andrewdudin1337@gmail.com>
2021-11-03 23:05:21 +0300

    Composite. Добавил первую фигуру: прямоугольник. Пока без отрисовки

--- lab7/Composite/src/Content/Shape/CRectangle/CRectangle.cpp
+++ lab7/Composite/src/Content/Shape/CRectangle/CRectangle.cpp
@@ -1,5 +1,21 @@
-//
-// Created by andrey on 02.11.2021.
-//
-
 #include "CRectangle.h"
+
+CRectangle::CRectangle(PointD point, double width, double height)
+	: m_frame({point, width, height})
+{
+}
+
+void CRectangle::Draw() const
+{
+	CShape::Draw();
+}
+
+FrameD CRectangle::GetFrame() const
+{
+	return m_frame;
+}
+
+void CRectangle::SetFrame(FrameD& frame)
+{
+	m_frame = frame;
+}
--- lab7/Composite/src/Content/Shape/CRectangle/CRectangle.h
+++ lab7/Composite/src/Content/Shape/CRectangle/CRectangle.h
@@ -1,7 +1,17 @@
 #pragma once
-#include "../IShape/IShape.h"
+#include "../CShape/CShape.h"
 
-class CRectangle : public IShape
+class CRectangle : public CShape
 {
+protected:
+	CRectangle(PointD point, double width, double height);
 
+	void Draw() const override;
+
+	FrameD GetFrame() const override;
+
+	void SetFrame(FrameD& frame) override;
+
+private:
+	FrameD m_frame;
 };
